b821cf1849b6d93d83b999ef7530a50f4f6f8f41
“Edu58” <edumuriithi58@gmail.com>
2022-06-04 22:48:00 +0300

    adds home, login & signup templates & views

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
