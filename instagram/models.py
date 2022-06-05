b08a95675d322872356e12f93b384e8e0a8524a9
“Edu58” <edumuriithi58@gmail.com>
2022-06-05 09:49:53 +0300

    fix: Profile __str__ return User of type None

--- instagram/models.py
+++ instagram/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     birth_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return self.user
+        return self.user.username
 
 
 class Image(models.Model):
