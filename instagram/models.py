7a57d86ca68f8f8b4fee2d2a7fe566a77bc70d6e
“Edu58” <edumuriithi58@gmail.com>
2022-06-05 07:41:57 +0300

    adds Users model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Users(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=50, null=False, blank=False)
