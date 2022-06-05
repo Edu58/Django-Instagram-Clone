5d9e350c29bb16b33f006f9c1d1b90a84ea0dc75
“Edu58” <edumuriithi58@gmail.com>
2022-06-05 08:56:39 +0300

    adds Comments model

--- instagram/models.py
+++ instagram/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class CustomUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    fullname = models.CharField(max_length=50, null=False, blank=False)
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(null=True, blank=True, upload_to='photos/')
     bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=200, null=False, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
-from instagram.models import CustomUser
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
