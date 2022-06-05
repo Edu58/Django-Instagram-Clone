8d43d16dbebf4ec2787797c28b1f98d10e3ae83c
“Edu58” <edumuriithi58@gmail.com>
2022-06-05 09:10:48 +0300

    adds Likes model

--- instagram/models.py
+++ instagram/models.py
@@ -10,7 +10,22 @@ class Profile(models.Model):
     birth_date = models.DateField(null=True, blank=True)
 
 
+class Image(models.Model):
+    image = models.ImageField(upload_to='photo/', blank=False, null=False)
+    image_name = models.CharField(max_length=50, null=False, blank=False)
+    image_caption = models.CharField(max_length=100, blank=False, null=False)
+    created_on = models.DateField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+
 class Comments(models.Model):
     comment = models.CharField(max_length=200, null=False, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    created_on = models.DateField(auto_now_add=True)
 
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
