c732a78580eedd592ffdce2efa10a71a21e7861d
“Edu58” <edumuriithi58@gmail.com>
2022-06-05 13:16:15 +0300

    adds required to signup form model

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,10 +1,13 @@
 from dataclasses import fields
 from pyexpat import model
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
+    username = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name','username', 'password1']
