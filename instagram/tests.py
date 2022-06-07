0591ae318a0b21ed64c092bfdf4ff83d5db6b5ac
“Edu58” <edumuriithi58@gmail.com>
2022-06-07 18:09:06 +0300

    adds tests

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,79 +1,73 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from .models import Profile, Image, Comments, Likes
-
-
+from datetime import date
 # Create your tests here.
+class TestUser(TestCase):
+    def setUp(self) -> None:
+        self.example_user = User(username="ed",email="ed@example.com",password="ed123123")
+        self.example_user.save()
+
+    def test_user_instance(self):
+        self.assertTrue(isinstance(self.example_user, User))
+
 class TestProfile(TestCase):
     def setUp(self) -> None:
-        self.example_category = Profile(user=1, profile_photo='./static/images/instahome.png', bio="This is my bio",
-                                        birth_date=2022 - 5 - 17, location='Kagumo')
+        self.example_user = User(username="ed",email="ed@example.com",password="ede43423443")
+        self.example_user.save()
+        
+        mock_date = date(2021, 3, 4)
 
+        self.example_profile = Profile(user = self.example_user,profile_photo = "./static/images/instahome.png",bio = "The developer", birth_date=mock_date, location="Westlands")
 
-class TestProfile(TestCase):
-    def test_profile(self):
-        self.assertTrue(isinstance(self.example_location, Locations))
+    def test_profile_instance(self):
+        self.assertTrue(isinstance(self.example_profile, Profile))
 
-# class TestImages(TestCase):
-#     def setUp(self) -> None:
-#         self.example_category = Categories(name="New Category")
-#         self.example_category.save()
-#
-#         self.example_location = Locations(name="Kenya")
-#         self.example_location.save()
-#
-#         self.example_image = Images(name="test image", description="This is a test for the Image model",
-#                                     location=self.example_location, category=self.example_category,
-#                                     image="../media/images/MG_1684.jpg")
-#
-#     def tearDown(self) -> None:
-#         Categories.objects.all().delete()
-#         Locations.objects.all().delete()
-#         Images.objects.all().delete()
-#
-#     def test_image_instance(self):
-#         self.assertTrue(isinstance(self.example_image, Images))
-#
-#     def test_save_image(self):
-#         self.example_image.save_image()
-#         all_images = Images.objects.all()
-#         self.assertTrue(len(all_images) == 1)
-#
-#     def test_update_image(self):
-#         self.example_image.save_image()
-#
-#         self.updated_category = Categories(name="Gaming")
-#         self.updated_category.save()
-#         self.updated_location = Locations(name="Kakamega")
-#         self.updated_location.save()
-#
-#         self.example_image.update_image(
-#             new_image="../media/images/IMG_6548-4.jpg",
-#             new_category=self.updated_category,
-#             new_name="New photo",
-#             new_location=self.updated_location,
-#             new_description="A new photo for testing update image"
-#         )
-#
-#         updated_image = self.example_image
-#
-#         self.assertEqual(self.example_image, updated_image)
-#
-#     def test_get_image_by_id(self):
-#         self.example_image.save_image()
-#         image = self.example_image.get_image_by_id(self.example_image.id)
-#         self.assertEqual(self.example_image, image)
-#
-#     def test_search_image(self):
-#         self.example_image.save_image()
-#         searched = self.example_image.search_image("New Category")
-#         self.assertTrue(len(searched) == 1)
-#
-#     def test_filter_by_location(self):
-#         self.example_image.save_image()
-#         searched = self.example_image.filter_by_location("Kenya")
-#         self.assertTrue(len(searched) == 1)
-#
-#     def test_delete_image(self):
-#         self.example_image.save_image()
-#         self.example_image.delete_image(self.example_image.id)
-#         self.assertTrue(len(Images.objects.all()) == 0)
+class TestPost(TestCase):
+    def setUp(self) -> None:
+        self.example_user = User(username="ed",email="ed@example.com",password="ede43423443")
+        self.example_user.save()
+
+        self.example_profile = Profile(user = self.example_user,profile_photo = "./static/images/instahome.png",bio = "The developer", birth_date=datetime.date(1990, 12, 17), location="Westlands")
+        self.example_profile.save()
+
+        self.example_post = Image(image_upload="./static/images/instahome.png",image_name="mypost",image_caption="Lorem ipsum",profile=self.example_profile)
+
+    def tearDown(self) -> None:
+        Profile.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_post_instance(self):
+        self.assertTrue(isinstance(self.example_post, Image))
+
+    def test_del_instance(self):
+        self.example_post.save()
+        self.example_post.delete()
+        self.assertTrue(len(Image.objects.all()) == 0)
+
+class TestComment(TestCase):
+    def setUp(self) -> None:
+        self.example_user = User(username="ed",email="ed@example.com",password="ede43423443")
+        self.example_user.save()
+
+        self.example_profile = Profile(user = self.example_user,profile_photo = "./static/images/instahome.png",bio = "The developer", birth_date=datetime.date(1990, 12, 17), location="Westlands")
+       
+        self.example_profile.save()
+
+        self.example_post = Image(image_upload="./static/images/instahome.png",image_name="mypost",image_caption="Lorem ipsum",profile=self.example_profile)
+        self.example_post.save()
+
+        self.example_comment = Comments(profile=self.example_profile, comment="Nice one", image=self.example_post)
+
+    def tearDown(self) -> None:
+        Profile.objects.all().delete()
+        Image.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_comment_instance(self):
+        self.assertTrue(isinstance(self.example_comment, Comments))
+
+    def test_del_comment(self):
+        self.example_comment.save()
+        self.example_comment.delete()
+        self.assertTrue(len(Comments.objects.all())==0)
